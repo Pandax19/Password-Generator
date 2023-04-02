5a28d8cdc870c90a0b785f84f5d10497b74f2934
Pandax19 <ajwhitley@comcast.net>
2023-04-01 23:59:47 -0400

    added some code and variables to be used in the generator

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,30 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-  passwordText.value = password;
+var lettersLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-}
+var lettersHigh = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "="]
+
+
+var pool = []
+pool = pool.concact(numbers, lettersLow, lettersHigh, symbols)
+console.log(pool)
+
+//randomizing arrays
+
+let lettersLowRandom = Math.floor(Math.random(lettersLow) * lettersLow.length)
+let lettersHighRandom = Math.floor(Math.random(lettersHigh) * lettersHigh.length)
+let numbersRandom = Math.floor(Math.random(numbers) * numbers.length)
+let symbolsRandom = Math.floor(Math.random(symbols) * symbols.length)
+
+
+// For (i=0; i<array.length; i++) { code to execute here}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

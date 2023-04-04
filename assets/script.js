8d9a815c630afd86d2ddf5b1e3cedeb0297d299c
Pandax19 <ajwhitley@comcast.net>
2023-04-03 22:26:41 -0400

    added more code, prompts and function to pass gen

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,45 @@ var lettersHigh = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "="]
 
-
-var pool = []
-pool = pool.concact(numbers, lettersLow, lettersHigh, symbols)
-console.log(pool)
-
 //randomizing arrays
 
 let lettersLowRandom = Math.floor(Math.random(lettersLow) * lettersLow.length)
 let lettersHighRandom = Math.floor(Math.random(lettersHigh) * lettersHigh.length)
 let numbersRandom = Math.floor(Math.random(numbers) * numbers.length)
 let symbolsRandom = Math.floor(Math.random(symbols) * symbols.length)
+
+var pool = [];
+
+
+function writePassword() {
+    var characters = prompt("Please select a length of password. \n" + "min=8 Max=128");
+    if(characters < 8 || characters > 128) {
+        alert("Password must be between 8 and 128 characters");
+        return;
+    } else {
+        alert("Password Length Acceptable.");
+        var capitalLetters = confirm("Use captial letters?");
+        var lowerCaseLetters = confirm("Use Lower case Letters?");
+        // foolow pattern for the other characters
+        for (let i =0; i < characters; i++) {
+            if(capitalLetters) {
+                pool.push(...lettersHighRandom);
+            }
+            if(lowerCaseLetters) {
+                pool.push(...lettersLowRandom)
+            }
+            if(nums) {
+                pool.push(...numbersRandom)
+            }
+            if(syms) {
+                pool.push(...symbolsRandom)
+            }
+        }
+    }
+}
+
+
+
 
 
 // For (i=0; i<array.length; i++) { code to execute here}
